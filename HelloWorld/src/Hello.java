716bd560884ea2723de2faa834234f444dd04717
Treadandcat <ghosttrio@naver.com>
2022-11-12 16:19:33 +0900

    과제

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+
+		String j="*";
+		for(int i=1;i<=10;i++){
+			for(j.length(); j.length()==i; j+="*") {
+				System.out.println(j);
+			}
+		}
+	}
+}
